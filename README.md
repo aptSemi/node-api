ed680ddb123ddac041be8ca289cefc7894038591
aptsemi <joemckellar399@gmail.com>
2022-11-30 15:26:57 -0800

    set up API server

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Project API in Node.  So I can learn more about how to do this stuff.
 
+nodemon - for autostarting server
--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const express = require("express");
 const cors = require("cors");
 const _ = require("lodash");
 const { v4: uuid } = require("uuid");
+
+const app = express();
+  
+app.get("/outfit", (req, res) => {
+  const shirts = ["Navy", "Black", "White", "Green"];
+  const pants = ["Gray", "Black", "Blue", "Plaid"];
+  const shoes = ["Brown", "Black", "Sperrys", "Chucks" ];
+
+  res.json({
+    shirts: _.sample(shirts),
+    pants: _.sample(pants),
+    shoes: _.sample(shoes)
+  });
+})
+
+app.listen(3000, () => console.log("API Server is running..."));
+
+
