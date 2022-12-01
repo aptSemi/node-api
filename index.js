0c7d14b2f34f3e3cded3fbe6806f8c11f3ecd89a
aptsemi <joemckellar399@gmail.com>
2022-11-30 16:59:50 -0800

    added cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { v4: uuid } = require("uuid");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
   
 app.get("/outfit", (req, res) => {
   const shirts = ["Navy", "Black", "White", "Green"];
@@ -19,6 +20,22 @@ app.get("/outfit", (req, res) => {
     shoes: _.sample(shoes)
   });
 })
+
+app.get("/comments/:id", async (req, res) => {
+  const id = req.params.id;
+  let content; 
+    
+  try {
+      content = await fs.readFile(`data/comments/${id}.txt`, "utf-8");
+  } catch (err) {
+      return res.sendStatus(404);
+  }
+
+  res.json({
+    content: content
+  });
+
+});
 
 app.post("/comments", async (req, res) => {
   const id = uuid();
